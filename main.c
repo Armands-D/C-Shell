0df3f2d33bef4e0a5fb688cb224c7defc8c23896
armands <armands.dunskis@gmail.com>
2022-06-01 22:22:40 +0100

    fixed overflow & add compile flags

--- main.c
+++ main.c
@@ -2,35 +2,35 @@
 #include <stdlib.h>
 
 char* readline(int *LINE_LEN);
-void reset(char **LINE, int *LINE_LEN);
+void reset_line(char **LINE, int *LINE_LEN);
 
 int main(int argc, char *argv[]){
   int STATUS = 1, LINE_LEN = 1;
-  char *PROMPT = (char*)malloc(4), *LINE = (char*)malloc(LINE_LEN);
+  char *PROMPT = (char*)malloc(4), *LINE = (char*)malloc(LINE_LEN + 1);
   PROMPT = "a/d> ";
   while(STATUS){
     printf("%s", PROMPT);
     LINE = readline(&LINE_LEN);
-    reset(&LINE, &LINE_LEN);
+    reset_line(&LINE, &LINE_LEN);
   }
   return STATUS;
 }
 
 char *readline(int *LINE_LEN){
   char c;
-  char *line = (char*)malloc(*LINE_LEN);
+  char *line = (char*)malloc(*LINE_LEN + 1);
   while(c != '\n'){
     c = getchar();
     line[*LINE_LEN] = c;
     (*LINE_LEN)++;
-    line = (char*)realloc(line, *LINE_LEN);
+    line = (char*)realloc(line, *LINE_LEN + 1);
   }
   line[*LINE_LEN] = '\0';
   return line;
 }
 
-void reset(char **LINE, int *LINE_LEN){
+void reset_line(char **LINE, int *LINE_LEN){
   *LINE_LEN = 1;
-  *LINE = NULL;
+  free(*LINE);
 }
 
--- makefile
+++ makefile
@@ -1,12 +1,12 @@
 FILES = main.c
-FLAGS = -Wall -Werror -fstack-protector -fsanitize=leak -fsanatize=address
+FLAGS = -Wall -Werror -fstack-protector -fsanitize=leak -fsanitize=address
 OUT = main
 CC = gcc
 
 all: clean compile run clean
 
 compile: $(FILES)
-	$(CC) $(FILES) -o $(OUT)
+	$(CC) $(FILES) -o $(OUT) $(FLAGS)
 
 run: $(OUT)
 	-./$(OUT)
